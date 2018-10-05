21ac53c5214630dd1f3038a27a4125d3ddd6af32
Liz Khuu <khuu.liz@gmail.com>
2018-10-05 15:38:40 -0400

    Switch from environment variables to keys variables

--- frontend/src/map-screen/search-map-container.js
+++ frontend/src/map-screen/search-map-container.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SearchMap from './search-map';
+import keys from '../keys';
 
 export default class SearchMapContainer extends React.Component {
     render() {
         return (
             <SearchMap
-				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${keys.GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
 				containerElement={<div style={{ height: `100%`, width: `100%` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
