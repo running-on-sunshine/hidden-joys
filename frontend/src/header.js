84069783901d1364297ab49bcc5bef3c85fe9b6e
Liz Khuu <khuu.liz@gmail.com>
2018-10-08 14:10:14 -0400

    Use navlinks for header

--- frontend/src/header.js
+++ frontend/src/header.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PlayDropdownMenu from './play-dropdown-menu';
 
 let Header = () =>
     <div className="header">
-        <a href="/#/" 
-            className="nav-item">
+        <NavLink to='/' className="nav-item">
             <p className="nav-item">Hidden Joys <i className="far fa-smile"></i> 
             </p>
-        </a>
+        </NavLink>
         <div className='header-right'>
-            <a href="/#/about"
-                className="nav-item">
+            <NavLink to='/about' className="nav-item">
                 <p className="nav-item">About</p>
-            </a>
+            </NavLink>
             <PlayDropdownMenu />
         </div>
     </div>
