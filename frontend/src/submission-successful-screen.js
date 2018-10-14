66e8774e86829355f7ea031fb1dd6f25c475a0e9
clinturbin <clinturbin@gmail.com>
2018-10-14 13:57:16 -0400

    Updated layout styling for Add Item Screen

--- frontend/src/submission-successful-screen.js
+++ frontend/src/submission-successful-screen.js
@@ -7,28 +7,33 @@ import ItemDescriptionButton from './item-description-button';
 import './stylesheets/add-item-screen.css';
 
 class SubmissionSuccessfulScreen extends React.Component {
-    constructor(props) {
-        super(props);
-    };
     render() {
         return (
             <div className="full-screen">
                 <Header />
-                <div className="screen form-screen success-image-background">
+                <div className="screen center-div success-image-background">
                     <div className='add-item-form'>
-                        <p className='form-title'>Thank You!!!</p>
-                        <div className='form-section'>
-                            <p className='form-text'>Thank you very much for adding some joy into the world!</p>
-                            <p className='form-text'>Your item id is:</p>
-                            <div className='input-box id-box'>
-                                <p className='id-text'>{this.props.foundCode}</p>
+                        <div className='form-header center-div'>
+                            <p className='form-title'>Thank You!!!</p>
+                        </div>
+                        <div className='form-body thank-you-body'>
+                            <div className='form-section'>
+                                <p className='form-text'>Thank you very much for adding some joy into the world!</p>  
+                            </div>
+                            <div className='form-section'>
+                                <p className='form-text'>Your item id is:</p>
+                                    <div className='input-box id-box'>
+                                        <p className='id-text'>{this.props.foundCode}</p>
+                                    </div>
+                                </div>
+                            <div className='form-section'>
+                                <p className='form-text'>Please include this id when hiding your item of joy so that others can let us know when they have found the item!</p>
+                            </div>
+                            <div className='form-section'>
+                                <ItemDescriptionButton id={this.props.itemId}/>
                             </div>
                         </div>
-                        <div className='form-section'>
-                            <p className='form-text'>Please include this id when hiding your item of joy so that others can let us know when they have found the item!</p>
-                            <ItemDescriptionButton id={this.props.itemId}/>
-                        </div>
-                        <div className='form-section'>
+                        <div className='form-footer center-div'>
                             <div className='form-btns-container'>
                                 <Link to='/add'><button className='form-button'>Add Joy</button></Link>
                                 <Link to='/search'><button className='form-button'>Find Joy</button></Link>
